9f964ca17acfc1a6e9d104da08e9f5b0c6c955b7
Satyam-1516 <str.satyam15@gmail.com>
2023-05-01 19:39:51 +0530

    Added Few more functions

--- src/test/java/org/example/AmazonEmail.java
+++ src/test/java/org/example/AmazonEmail.java
@@ -2,12 +2,16 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.Set;
 
 public class AmazonEmail {
     ChromeDriver driver = new ChromeDriver();
@@ -22,13 +26,15 @@ public class AmazonEmail {
     }
     @Test
     public void getTitleFromPage() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.findElement(By.xpath("//*[@id=\"ap_email\"]")).sendKeys("str.life.satyam@gmail.com");
         driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys("Str@1516");
         driver.findElement(By.xpath("//*[@id=\"signInSubmit\"]")).click();
+
+        Thread.sleep(15000);
         ////////HOVER////////
         WebElement ele = driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span"));
-
         //Creating object of an Actions class
         Actions action = new Actions(driver);
 
@@ -37,31 +43,84 @@ public class AmazonEmail {
 ///////////////////////////////////////////////////////////////////////
         /// CREATING WISHLIST
         driver.findElement(By.xpath("//span[normalize-space()='Create a Wish List']")).click();
-        Thread.sleep(1000);
+        Thread.sleep(8000);
         driver.findElement(By.xpath("//input[@aria-labelledby='createList-announce']")).click();
-        Thread.sleep(4000);
+        Thread.sleep(8000);
 
         driver.findElement(By.xpath("//input[@id='list-name']")).clear();
-        Thread.sleep(4000);
+        Thread.sleep(8000);
 
         driver.findElement(By.xpath("//input[@id='list-name']")).sendKeys("My WishList");
-        Thread.sleep(4000);
+        Thread.sleep(8000);
         driver.findElement(By.xpath("//*[@id=\"wl-redesigned-create-list\"]/span/span/input")).click();
+        Thread.sleep(8000);
+        // WISHLIST NAME EDIT
+//        driver.findElement(By.xpath("//div[@class='aok-inline-block aok-align-center'][normalize-space()='More']")).click();
+//        driver.findElement(By.xpath("//a[@id='editYourList']")).click();
+//
+//        Thread.sleep(5000);
+//
+//        driver.findElement(By.xpath("//input[@id='list-settings-name']")).clear();
+//        Thread.sleep(8000);
+//        driver.findElement(By.xpath("//input[@id='list-name']")).sendKeys("My WishList 1");
+//        Thread.sleep(8000);
 
 
         //// ITEM SEARCH CLICK
         Thread.sleep(4000);
-        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Mouse");
-        Thread.sleep(10000);
+        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("DEATH: AN INSIDE STORY");
+
+                Thread.sleep(10000);
         driver.findElement(By.cssSelector("#nav-search-submit-button")).click();
-        Thread.sleep(8000);
+        Thread.sleep(10000);
 
         /// ITEM CLICK
-        driver.findElement(By.xpath("//div[contains(@class,'rush-component s-featured-result-item')]//span[@class='a-size-medium a-color-base a-text-normal'][contains(text(),'HP X1000 Wired USB Mouse with 3 Handy Buttons, Fas')]")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("//input[@id='add-to-wishlist-button']")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("//span[@id='atwl-list-privacy-12F0BI9R2UJA5']"));
-        Thread.sleep(4000);
+        // commented out below line because due ads unable yo click the desired link
+        //driver.findElement(By.cssSelector("div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1'] h2[class='a-size-mini a-spacing-none a-color-base s-line-clamp-2'] span:nth-child(4)")).click();
+        Thread.sleep(8000);
+        String parent=driver.getWindowHandle();
+
+        Set<String> s=driver.getWindowHandles();
+
+        // Now iterate using Iterator
+        Iterator<String> I1= s.iterator();
+
+        while(I1.hasNext()) {
+
+            String child_window = I1.next();
+
+
+            if (!parent.equals(child_window)) {
+                driver.switchTo().window(child_window);
+
+                System.out.println(driver.switchTo().window(child_window).getTitle());
+                driver.findElement(By.name("submit.add-to-registry.wishlist")).click();
+
+                Thread.sleep(6000);
+                driver.findElement(By.xpath("//*[@id=\"continue-shopping\"]/span/span/input")).click();
+                driver.close();
+
+            }
+            driver.switchTo().window(parent);
+        }
+/// AFTER ADD TO WISHLIST SECTION
+        WebElement ele1 = driver.findElement(By.xpath("//span[normalize-space()='Account & Lists']"));
+        Actions action1 = new Actions(driver);
+        //Performing the mouse hover action on the target element.
+        action1.moveToElement(ele1).perform();
+        Thread.sleep(8000);
+        driver.findElement(By.xpath("//span[normalize-space()='My WishList']")).click();
+        Thread.sleep(8000);
+
+  //// DELETE ITEM FROM WISHLIST
+        driver.findElement(By.xpath("//input[@name='submit.deleteItem']")).click();
+  ///// DELETE WISHLIST
+        driver.findElement(By.xpath("//div[@class='aok-inline-block aok-align-center'][normalize-space()='More']")).click();
+        driver.findElement(By.xpath("//a[@id='editYourList']")).click();
+        Thread.sleep(5000);
+        js.executeScript("window.scrollBy(0,360)");
+        driver.findElement(By.cssSelector("span[class='a-button a-spacing-base a-button-base full-width-element a-button-focus'] input[type='submit']")).click();
+        // above line failed because we scroll down the page but we need to scroll the pop upi.e the wishlist popup.
+
     }
 }
